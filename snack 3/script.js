63d387ae8a644625a28920693bfdd58fdc79adfa
feffe1702 <fpm.asd@gmail.com>
2024-10-11 14:58:28 +0200

    [DONE] complete ex3

--- snack 3/script.js
+++ snack 3/script.js
@@ -2,3 +2,20 @@
 Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
  */
+
+// const numeriVari = [];
+// let numA;
+// let numB;
+
+function confrontoNumeri(array , numeroA , numeroB) {
+    const numeriCompresi = [];
+    for (let i = 0; i < array.length; i++) {
+        if(array[i] > numeroA && array[i] < numeroB){
+            numeriCompresi.push(array[i])
+        }
+        
+    };
+    return numeriCompresi;
+}
+
+console.log(confrontoNumeri([62 , 9 , 43 , 15 , 12 , 13 , 32 , 25 ,] , 11 , 35));
