ff21beecde2d4eeb87cefe719d05b8aed3d4da7c
feffe1702 <fpm.asd@gmail.com>
2024-10-11 14:34:21 +0200

    [DONE] complete ex2

--- snack 2/script.js
+++ snack 2/script.js
@@ -5,3 +5,50 @@ Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
  */
+
+const squadre = [
+    {
+        nome : "roma",
+        "punti fatti" : 0,
+        "falli subiti" : 0,
+    },
+    {
+        nome : "atalanta",
+        "punti fatti" : 0,
+        "falli subiti" : 0,
+    },
+    {
+        nome : "lazio",
+        "punti fatti" : 0,
+        "falli subiti" : 0,
+    },
+    {
+        nome : "juventus",
+        "punti fatti" : 0,
+        "falli subiti" : 0,
+    },
+    {
+        nome : "inter",
+        "punti fatti" : 0,
+        "falli subiti" : 0,
+    },
+    {
+        nome : "milan",
+        "punti fatti" : 0,
+        "falli subiti" : 0,
+    },
+];
+
+const squadreAggiornate = [];
+
+function generaValori() {
+    for (let i = 0; i < squadre.length; i++) {
+        squadre[i]["punti fatti"] = Math.floor(Math.random() * 100) + 1;
+        squadre[i]["falli subiti"] = Math.floor(Math.random() * 100) + 1;
+        squadreAggiornate.push(squadre[i]);
+        
+    }
+    return squadreAggiornate;
+}
+
+console.log(generaValori());
