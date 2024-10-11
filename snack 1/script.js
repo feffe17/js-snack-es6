051854e6ec7cf034a0d1cdae415a451e24b5c9a0
feffe1702 <fpm.asd@gmail.com>
2024-10-11 13:56:41 +0200

    [WIP] definizione array

--- snack 1/script.js
+++ snack 1/script.js
@@ -2,3 +2,38 @@
 Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore.*/
+
+const bici = [
+    {
+        nome : "bici 1",
+        peso : 43,
+    },
+    {
+        nome : "bici 2",
+        peso : 23,
+    },
+    {
+        nome : "bici 3",
+        peso : 63,
+    },
+    {
+        nome : "bici 4",
+        peso : 98,
+    },
+    {
+        nome : "bici 5",
+        peso : 19,
+    },
+    {
+        nome : "bici 6",
+        peso : 33,
+    },
+    {
+        nome : "bici 7",
+        peso : 3,
+    },
+    {
+        nome : "bici 1",
+        peso : 5,
+    },
+]
